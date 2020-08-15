06fbf30eb78f7f2e0c37090a77e44398e01f5f14
Yueshi Shen <yuesshen@amazon.com>
2020-08-15 16:31:11 -0700

    work in progress: 1) update JS to the latest IVS player SDK, 2) remove jquery dependency, 3) support arbitary number of answers in the quiz, 4) add the test stream's playback URL, 5) create README.md for the player

---
+++ web/IVSplayer/README.md
@@ -0,0 +1,4 @@
+// Note: Search the key word "QoS event" for the implementation of constructing
+//   and sending QoS playback events. Also, search the key word "timed metadata
+//   feedback event" for that of quiz answer events. Sample backend implementation
+//   is documented at https://code.amazon.com/packages/StarfruitQoS
--- web/IVSplayer/js/config.js
+++ web/IVSplayer/js/config.js
@@ -1,6 +1,10 @@
 export const config = {
-  "sendQoSEventUrl": "PLAYER_SUMMARY_ENDPOINT",
-  "sendQuizAnswerUrl":"ANSWER_SUMMARY_ENDPOINT",
+  // live channel's playback URL, 893648527354.channel.xhP3ExfcX8ON.m3u8 is a permanent test channel with quiz timed metadata
+  "PlaybackURL": "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.xhP3ExfcX8ON.m3u8",
+  // end point for sending player QoS (i.e., playback summary) events
+  "SendQoSEventURL": "",
+  // end point for sending timed metadata feedback (e.g., quiz answer) events 
+  "SendQuizAnswerURL":"",
+  // ask Jai
   "DeliveryStreamName":"DELIVERY_STREAM_NAME",
-  "LiveURL": "LIVE_URL"
 };
