6570a08051f4a5fcb3febb7367fbc48ce1255ac8
Jaiganesh Girinathan <ganeshji@amazon.com>
2020-08-18 10:51:21 +0400

    added jquery support, config.js fixes for variables

--- web/IVSplayer/js/config.js
+++ web/IVSplayer/js/config.js
@@ -2,7 +2,7 @@ export const config = {
   // an IVS live channel (or VOD title)'s playback URL, 893648527354.channel.xhP3ExfcX8ON.m3u8 is a permanent test channel with quiz timed metadata
   "PlaybackURL": "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.xhP3ExfcX8ON.m3u8",
   // (ask Jai) end point for sending playback QoS (i.e., playback summary) events
-  "SendQoSEventURL": "",
-  // (ask Jai) end point for sending timed metadata feedback (e.g., quiz answer) events 
-  "SendQuizAnswerURL":"",
+  "SendQoSEventURL": "PLAYER_SUMMARY_ENDPOINT",
+  // (ask Jai) end point for sending timed metadata feedback (e.g., quiz answer) events
+  "SendQuizAnswerURL":"ANSWER_SUMMARY_ENDPOINT",
 };
