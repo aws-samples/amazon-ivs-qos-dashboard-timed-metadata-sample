d5998678fc05160fe103bf3eb72280db64b9b891
Jaiganesh Girinathan <ganeshji@amazon.com>
2020-08-19 12:35:16 +0400

    ES fixes for QUIZ_ANSWER metric, naming convention of PlaybackURL

--- web/IVSplayer/js/config.js
+++ web/IVSplayer/js/config.js
@@ -1,6 +1,6 @@
 export const config = {
   // an IVS live channel (or VOD title)'s playback URL, 893648527354.channel.xhP3ExfcX8ON.m3u8 is a permanent test channel with quiz timed metadata
-  "PlaybackURL": "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.xhP3ExfcX8ON.m3u8",
+  "PlaybackURL": "PLAYBACK_URL",
   // (ask Jai) end point for sending playback QoS (i.e., playback summary) events
   "SendQoSEventURL": "PLAYER_SUMMARY_ENDPOINT",
   // (ask Jai) end point for sending timed metadata feedback (e.g., quiz answer) events
