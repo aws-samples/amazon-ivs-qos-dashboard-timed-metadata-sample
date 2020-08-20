7ba3c222d0e656cf31d0edf33a575a96f26748b9
Yueshi Shen <yuesshen@amazon.com>
2020-08-20 15:13:41 -0700

    cosmetic change on comments

--- web/IVSplayer/js/config.js
+++ web/IVSplayer/js/config.js
@@ -1,8 +1,9 @@
 export const config = {
-  // an IVS live channel (or VOD title)'s playback URL, 893648527354.channel.xhP3ExfcX8ON.m3u8 is a permanent test channel with quiz timed metadata
+  // an IVS live channel (or VOD title)'s playback URL, for example, the permanent test channel with quiz timed metadata
+  //   https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.xhP3ExfcX8ON.m3u8
   "PlaybackURL": "PLAYBACK_URL",
-  // (ask Jai) end point for sending playback QoS (i.e., playback summary) events
+  // end point for sending playback QoS (i.e., playback summary) events
   "SendQoSEventURL": "PLAYER_SUMMARY_ENDPOINT",
-  // (ask Jai) end point for sending timed metadata feedback (e.g., quiz answer) events
+  // end point for sending timed metadata feedback (e.g., quiz answer) events
   "SendQuizAnswerURL":"ANSWER_SUMMARY_ENDPOINT",
 };
