0c4c1d85339b47bcd296127270b7651c96514aff
Jaiganesh Girinathan <ganeshji@amazon.com>
2020-10-10 17:13:16 +0400

    broadcast metrics changes, ElasticSearch Dashboard changes

--- lambda-functions/event-bridge-handler-function/function.py
+++ lambda-functions/event-bridge-handler-function/function.py
@@ -15,18 +15,28 @@ def handler(event, context):
     print("Event :{}".format(event))
 
     metric_type = normalise_string(event["detail"]["event_name"])
-    channel_name = parse_channel_name(event["resources"][0])
-    print("Channel Name :%{}", channel_name)
-    print("metricType :{}".format(metric_type))
 
     #array to hold multiple events
     container = []
     #to hold the event attributes
+    live_count = 0
+
+    # if the event is for status update we just need to send the current channel concurrency
+    if(metric_type == "STATUS_UPDATE"):
+        live_count = get_channnel_concurrency()
+        container.append(prepare_concurrency_metric(live_count))
+        print("STATUS_UPDATE container :{}".format(container))
+        return container
+
+    # for other possible events we compute concurrency, starvation time, broadcast time and fire the event types
     payload = {}
     payload['metric_type'] = metric_type
     payload['channel_watched'] = channel_name
     payload['region'] = event["region"]
-    live_count = 0
+
+    channel_name = parse_channel_name(event["resources"][0])
+    print("Channel Name :%{}", channel_name)
+    print("metricType :{}".format(metric_type))
 
     if(metric_type == "STREAM_START"):
         put_metric(channel_name, int(time.time()))
@@ -69,14 +79,20 @@ def prepare_concurrency_metric(live_count):
     return payload
 
 # status specifies whether we need to increment the concurrency count or decrement it
-def update_channel_concurrency(status):
-    concurrency_key = "CONCURRENCY"
-    if(status):
-        live_count = int(get_metric(concurrency_key)) + 1
+def update_channel_concurrency(increment):
+    concurrency = get_channnel_concurrency()
+
+    if(increment):
+        live_count = concurrency + 1
     else:
-        live_count = max(int(get_metric(concurrency_key)) - 1,0)
+        live_count = max(concurrency - 1,0)
     put_metric(concurrency_key,live_count)
     return live_count
+
+def get_channnel_concurrency():
+    concurrency_key = "CONCURRENCY"
+    concurrency = int(get_metric(concurrency_key))
+    return concurrency
 
 def parse_channel_name(arn_str):
     return arn_str[arn_str.rfind("/") + 1:]
